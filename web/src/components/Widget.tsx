bafd768264f945440480226197ca37e44e53996f
Victoria Lucena <lucenavictoria0@gmail.com>
2022-06-10 22:24:47 -0300

    correção bg widget

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -11,7 +11,7 @@ return (
  <Popover className="absolute bottom-4 right-4 md:right-8 md:bottom-8 flex-col items-end">
   {<Popover.Panel><WidgetForm/></Popover.Panel>}
 
-    <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group bg-pink">
+    <Popover.Button type className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
     <ChatTeardropDots className="w-6 h-6"/>
     <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
      <span className="pl-2"></span>
